049cb05a463d0b3e0f8ba818a5a3e6e892113a78
Gokul-0hno <85026979+Gokul-0hno@users.noreply.github.com>
2022-11-01 18:48:14 +0530

    Day 03

---
+++ checklist.md
@@ -0,0 +1,24 @@
+[ ] - Front End    
+    [x] - Build template for **products page**.
+        [x] - Build static front end.
+        [x] - Update front end to change state based on hardcoded data.
+
+    [ ] - Build static **home page**.
+
+    [ ] - Template for Cart
+        [ ] - Static front end.
+        [ ] - Update front end to change state based on hardcoded data.
+
+    [ ] - Template Checkout Page
+        [ ] - Static front end.
+        [ ] - Update front end to change state based on hardcoded data.
+
+
+[ ] - **Backend**
+    [ ] - Fetch preset categories API.
+    [ ] - Indexing.
+    [ ] - Search API.
+    [ ] - Linking Cart to checkout page.
+    [ ] - DBMS for customer info.
+
+
--- products_page/src/index.js
+++ products_page/src/index.js
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom/client'
 import { Footer } from './components';
 import './index.css'
 
-function ProductPage() {
-    return (
-        <>
-            <SearchBar />
-            <Results />
-            
-        </>
-    );
+const data = [
+    {"name": "Matta Palada Payasam Mix", "quantity": "250gms", "description": "Lorem Ipsum Dolores Sit Amet", "cost": "70"},
+    {"name": "Multi Millet Payasam Mix", "quantity": "500gms", "description": "Lorem Ipsum Dolores Sit Amet", "cost": "79.09"},
+    {"name": "Adapradhaman Payasam Mix", "quantity": "250gms", "description": "Lorem Ipsum Dolores Sit Amet", "cost": "77.27"},
+    {"name": "Sadhya Palada Payasam Mix", "quantity": "200gms", "description": "Lorem Ipsum Dolores Sit Amet", "cost": "80"},
+    {"name": "Sadhya Palada Payasam Mix", "quantity": "500gms", "description": "Lorem Ipsum Dolores Sit Amet", "cost": "215"},
+    {"name": "Rava Kesari Mix", "quantity": "200gms", "description": "Lorem Ipsum Dolores Sit Amet", "cost": "65"},
+]
+
+
+class ProductPage extends React.Component{
+    render() {
+        return (
+            <>
+                <SearchBar />
+                <Results results={this.props.data}/>
+
+            </>
+        );
+    }
 }
 
 
@@ -23,11 +35,11 @@ function SearchBar() {
     );
 }
 
-function Results() {
+function Results(props) {
     return (
         <>
             <ResultBanner resultText="Sweets" />
-            <ResultsGrid />
+            <ResultsGrid list={props.results}/>
         </>
     );
 }
@@ -51,31 +63,30 @@ function ResultBanner(props) {
 
 }
 
-function ResultsGrid() {
-    return (
+class ResultsGrid extends React.Component {
+    render() {
+        let products = [];
+        this.props.list.forEach((product) => {
+            products.push(<ProductCard itemId={product.name} cost={product.cost} quantity={product.quantity} desc={product.description} key={product.name}/>);
+        });
+        return(
         <div className='results-grid'>
-            <ProductCard itemId='Adapradhaman - 250gms' cost='900'/>
-            <ProductCard itemId='Palada - 500gms' cost='900'/>
-            <ProductCard itemId='Payasam' cost='900'/>
-            <ProductCard itemId='Payasam' cost='900'/>
-            <ProductCard itemId='Payasam' cost='900'/>
-            <ProductCard itemId='Payasam' cost='900'/>
-            <ProductCard itemId='Payasam' cost='900'/>
-            <ProductCard itemId='Payasam' cost='900'/>
-            <ProductCard itemId='Payasam' cost='900'/>
+           {products} 
         </div>
-    );
+        );
+    }
 }
 
 class ProductCard extends React.Component {
     render() {
         return (
-            <div className='product-card'>
+            <div className='product-card' id={this.props.itemId}>
                 <img className='product-image' src='https://placehold.jp/300x150.png' alt={this.props.itemId}/>
             
                 <div className='product-desc'>
                     <h3>{this.props.itemId}</h3>
-                    <p>lorem ipsum dolores sit amet</p>
+                    <p>{this.props.desc}</p>
+                    <h4>Weight: {this.props.quantity}</h4>
                 </div>
 
                 <ProductActions cost={this.props.cost} />
@@ -94,7 +105,7 @@ function ProductActions(props) {
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ProductPage />)
+root.render(<ProductPage data={data}/>)
 
 const form = ReactDOM.createRoot(document.getElementById('footer-form'));
 form.render(<Footer />);
