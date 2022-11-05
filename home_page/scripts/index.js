e4580bf4ed8d7934c80207b0b754296e8a43d518
Gokul-0hno <85026979+Gokul-0hno@users.noreply.github.com>
2022-11-05 19:46:06 +0530

    day 06 carousel animation complete

--- home_page/scripts/index.js
+++ home_page/scripts/index.js
@@ -5,28 +5,44 @@ const items = document.getElementsByClassName("carousel-item")
 const items_len = items.length;
 
 let curr_item = 0;
-items.item(0).classList.add("carousel-focus");
+let last_item = items_len - 1;
+let next_item = 1;
+items.item(last_item).classList.add("carousel-left");
+items.item(curr_item).classList.add("carousel-focus");
+items.item(next_item).classList.add("carousel-right");
 
 function clickLeft(){
+    items.item(last_item).classList.remove("carousel-left")
     items.item(curr_item).classList.remove("carousel-focus");
-    if (curr_item === 0){
-        curr_item = items_len - 1;
+    items.item(next_item).classList.remove("carousel-right");
+    next_item = curr_item;
+    curr_item = last_item;
+    if (last_item === 0){
+        last_item = items_len - 1;
     }
     else{
-        curr_item = curr_item - 1;
+        last_item = last_item - 1;
     }
+    items.item(last_item).classList.add("carousel-left")
     items.item(curr_item).classList.add("carousel-focus");
+    items.item(next_item).classList.add("carousel-right");
 }
 
 function clickRight() {
+    items.item(last_item).classList.remove("carousel-left")
     items.item(curr_item).classList.remove("carousel-focus");
-    if (curr_item === items_len - 1){
-        curr_item = 0;
+    items.item(next_item).classList.remove("carousel-right");
+    last_item = curr_item;
+    curr_item = next_item;
+    if (next_item === items_len - 1) {
+        next_item = 0;
     }
     else{
-        curr_item = curr_item + 1;
+        next_item = next_item + 1;
     }
+    items.item(last_item).classList.add("carousel-left")
     items.item(curr_item).classList.add("carousel-focus");
+    items.item(next_item).classList.add("carousel-right");
 }
 
 left_btn.addEventListener("click", clickLeft);
